ae3ec7937b10f26668b9e2b68b88b15e3fb9c9a2
Hafizbe <hafizbe>
2014-05-28 17:48:52 +0200

    Add Learning step

--- js/controllers/learning_c.js
+++ js/controllers/learning_c.js
@@ -1,10 +1,69 @@
 app.controller('learning_c', ['$scope', function($scope) {
 
+    $scope.disableBtnBack = true;
+    $scope.disableBtnNext = false;
+    $scope.step = 1 ;
+    $scope.steps_selected = [false,false,false,false];
+    $scope.response = false;
 
-    $scope.step = 0 ;
+    $scope.aRepondu = function(step){
 
+        // On passe par ce tableau car cela permet 
+        $scope.steps_selected[step-1] = true;
+        $scope.response = true;
+    }
+
+    $scope.detectActive = function(step){
+        if($scope.step == step)
+            return "active";
+        else if($scope.step > step)
+            return "complete";
+    }
+
+    $scope.nextStep = function(){
+        $scope.step++;
+        $scope.response = false;
+    }
+
+    $scope.previousStep = function(){
+        $scope.step--;
+        $scope.steps_selected[$scope.step - 1] = false;
+        $scope.response = false;
+    }
+
+    $scope.switchToStep = function(step)
+    {   
+        if($scope.step != 4)
+        {
+            // TODO //
+        }
+        $scope.step = step;
+    }
+
+    
+    $scope.$watch('step',function(step){
+        if(step == 1)
+        {
+            $scope.disableBtnBack = true;
+            $scope.disableBtnNext = false;
+        }
+        else if(step == 4){
+            $scope.disableBtnNext = true;
+            $scope.disableBtnBack = false;
+        }
+        else
+        {
+            $scope.disableBtnNext = false;
+            $scope.disableBtnBack = false;
+        }
+            
+    });
+
+
+    // Initialisation du plugin JS
     $scope.$on('$viewContentLoaded', function(){
-        var $validation = false;
+
+        /*var $validation = false;
         $('#fuelux-wizard')
             .ace_wizard({
                 //step: 2 //optional argument. wizard will jump to step "2" at first
@@ -15,7 +74,8 @@ app.controller('learning_c', ['$scope', function($scope) {
                 }
             })
             .on('finished', function(e) {
-                /*bootbox.dialog({
+                alert("ok terminé");
+                bootbox.dialog({
                  message: "Thank you! Your information was successfully saved!",
                  buttons: {
                  "success" : {
@@ -23,10 +83,11 @@ app.controller('learning_c', ['$scope', function($scope) {
                  "className" : "btn-sm btn-primary"
                  }
                  }
-                 });*/
+                 });
             }).on('stepclick', function(e){
+                alert("ok");
                 //e.preventDefault();//this will prevent clicking and selecting steps
-            });
+            });*/
     });
 
 }]);
