8c75ab2ba3d271f0e6884428a7d3b813673f47e3
Be Hafiz <hafizbe@gmail.com>
2014-05-29 13:53:57 +0200

    Creation service Card

---
+++ js/services/card_service.js
@@ -0,0 +1,11 @@
+app.service("cardService", ["$http",
+    function($http) {
+
+       this.getCard = function(card_id){
+           
+       }
+
+    }]);
+/**
+ * Created by adel on 29/05/2014.
+ */
